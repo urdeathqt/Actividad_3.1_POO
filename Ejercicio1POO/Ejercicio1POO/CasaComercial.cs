80c36e25be3f42d58e1ba81512ed0042085c3574
urdeathqt <ediken994@gmail.com>
2023-05-05 22:17:50 -0600

    segundo commit

---
+++ Ejercicio1POO/Ejercicio1POO/CasaComercial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1POO
+{
+    class CasaComercial
+    {
+        private Vehiculo[] vehiculos;
+        private int limite;
+        private int totalVehiculos;
+
+        public CasaComercial(int limite)
+        {
+            this.Vehiculos = new Vehiculo[limite];
+            this.Limite = limite;
+            totalVehiculos = 0;
+        }
+
+        public int Limite { get => limite; set => limite = value; }
+        internal Vehiculo[] Vehiculos { get => vehiculos; set => vehiculos = value; }
+        public int TotalVehiculos { get => totalVehiculos; set => totalVehiculos = value; }
+
+        public void aniadirVehiculo (Vehiculo v)
+        {
+            if (v != null && totalVehiculos < vehiculos.Length)
+            {
+                vehiculos[totalVehiculos] = v;
+                totalVehiculos++;
+            }
+
+        }
+
+        public void mostrarVehiculos ()
+        {
+            for (int i = 0; i < totalVehiculos; i++)
+            {
+                Console.WriteLine(vehiculos[i].ToString());
+            }
+        }
+
+        public void vaciarVehiculos ()
+        {
+            this.vehiculos = new Vehiculo[limite];
+            totalVehiculos = 0;
+        }
+
+        public void eliminarVehiculos(Vehiculo v)
+        {
+            if (v!= null && totalVehiculos != 0)
+            {
+                int posicion = -1;
+
+                for (int i = 0; i < totalVehiculos; i++)
+                {
+                    if (v.Id == vehiculos[i].Id)
+                    {
+                        posicion = i;
+                    }
+                }
+
+                if (posicion == -1)
+                {
+                    Console.WriteLine("Vehiculo no encontrado");
+                }
+                else
+                {
+                    vehiculos[posicion] = null;
+
+                    for (int i = posicion; i < totalVehiculos; i++)
+                    {
+                        vehiculos[i] = vehiculos[i + 1];
+                    }
+                    totalVehiculos--;
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio1POO/Ejercicio1POO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CasaComercial casaComercial = new CasaComercial(10);
+
+            Vehiculo v1 = new Vehiculo(1, "Mitsubishi", "Eclipse Cross", 2021, 30000);
+            Vehiculo v2 = new Vehiculo(2, "Toyota", "Hilux", 2019, 21000);
+            Vehiculo v3 = new Vehiculo(3, "Hyundai", "Santa fe", 2022, 27500);
+            Vehiculo v4 = new Vehiculo(4, "Ford", "Raptor", 2023, 31750);
+            Vehiculo v5 = new Vehiculo(5, "Honda", "CR-V", 2023, 28999);
+
+            casaComercial.aniadirVehiculo(v1);
+            casaComercial.aniadirVehiculo(v2);
+            casaComercial.aniadirVehiculo(v3);
+            casaComercial.aniadirVehiculo(v4);
+            casaComercial.aniadirVehiculo(v5);
+
+            Console.WriteLine("Se muestras todos los vehiculos: ");
+            casaComercial.mostrarVehiculos();
+
+            
+            casaComercial.eliminarVehiculos(v1);
+            casaComercial.eliminarVehiculos(v4);
+
+            Console.WriteLine("Vehiculos eliminados...");
+            casaComercial.mostrarVehiculos();
+
+            Console.WriteLine("Vacio");
+            casaComercial.vaciarVehiculos();
+
+            casaComercial.mostrarVehiculos();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
